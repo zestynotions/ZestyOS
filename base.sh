824102adf18f092a305bf12db082570c3460ee4d
Zesty <83487277+zestynotions@users.noreply.github.com>
2022-07-10 13:15:51 +0900

    new basefile

--- base.sh
+++ base.sh
@@ -23,7 +23,8 @@ echo ===========================================================
 ping -c1 google.com && startinstall=y || echo Failure!
 echo ""
 
-echo The script will partition you /dev/sda drive and format it !WARNING! all will be lost so you better be sure before continuing. You have been warned!...
+echo The script will partition you /dev/sda drive and format it 
+echo !WARNING! all will be lost so you better be sure before continuing...
 echo ================================================================================
 sleep 5 
 
@@ -61,12 +62,6 @@ sudo mount /dev/sda2 /mnt
 sudo mkdir /mnt/boot
 sudo mount /dev/sda1 /mnt/boot
 
-echo Now basestrapping the system ...
-sleep 2 
-sudo basestrap /mnt base base-devel runit elogind-runit linux linux-firmware glibc nano
-echo ===========================================================
-sleep 1
-
 echo Now Generating the fstab file for mountpoints ...
 echo ===========================================================
 sleep 3
@@ -75,41 +70,6 @@ sudo touch /mnt/etc/fstab
 sudo chmod 777 /mnt/etc/fstab
 sudo fstabgen -U /mnt >> /mnt/etc/fstab
 
-echo Copying the install script to the new root environtment ... 
-echo ===========================================================
-sleep 1
-# bring install file to new env
-sudo curl -LO zestynotions.com/zns/artix_base_install_final.sh
-sudo chmod 777 artix_base_install_final.sh
-sudo mv artix_base_install_final /mnt
-echo ""
-
-clear
-
-# prep rc.local to set static IP
-echo Time to add a network configuration, we will assume dhcp but you can change the /etc/rc.local later if you want it to be static.
-sleep 3
-ip link
-echo ""
-echo Add the name of your network adaptor e.g. "enp0s3" for virtualbox machines and likely "eth0" for normal lan.
-echo -e "\e[1;32m ============================================ \e[0m "
-read -p 'Name of network adaptor: ' netname
-echo -e "\e[1;32m ============================================ \e[0m "
-sudo touch /mnt/etc/rc.local
-sudo chmod 777 /mnt/etc/rc.local
-sudo echo "#ip link set $netname up" >> /mnt/etc/rc.local
-sudo echo "#ip addr add your_ip_here/24 brd + dev $netname" >> /mnt/etc/rc.local
-sudo echo "#ip route add default via router_ip_here" >> /mnt/etc/rc.local
-sudo echo "dhcpcd $netname" >> /mnt/etc/rc.local
-
-
-# reminder for after chroot action
-echo =============================================================================
-echo -e "Type \e[1;32m sh artix_final.sh \e[0m"
-echo =============================================================================
-sleep 3
-# change root environtment
-artix-chroot /mnt
 
 else
 
