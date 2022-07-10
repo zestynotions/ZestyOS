179afb6cd9d9ae54b41bf1fce572c9315a709f5b
Zesty <83487277+zestynotions@users.noreply.github.com>
2022-07-10 12:37:27 +0900

    updated initial artix install script

--- artix_base.sh
+++ artix_base.sh
@@ -3,11 +3,22 @@
 echo ====================================================
 echo ==== Artix Linux Install Script by ZestyNotions ====
 echo ====================================================
+sleep 3
 echo ""
 echo ""
+if [ "$(id -u)" = 0 ]; then
+    echo "##################################################################"
+    echo "This script MUST NOT be run as root user since it makes changes"
+    echo "to the \$HOME directory of the \$USER executing this script."
+    echo "The \$HOME directory of the root user is, of course, '/root'."
+    echo "We don't want to mess around in there. So run this script as a"
+    echo "normal user. You will be asked for a sudo password when necessary."
+    echo "##################################################################"
+    exit 1
+fi
 echo The script will assume you have your boot and root disk mounted and formatted...
 echo ================================================================================
-sleep 1
+sleep 3
 echo ""
 echo will quickly check your network connection ...
 echo ===========================================================
@@ -18,13 +29,14 @@ export startinstall
 # now check if $startinstall is "y"
 if [ "$startinstall" = "y" ]; then
 echo Now basestrapping the system ...
+sleep 3
 sudo basestrap /mnt base base-devel runit elogind-runit linux linux-firmware glibc nano
 echo ===========================================================
 sleep 1
 
 echo Now Generating the fstab file for mountpoints ...
 echo ===========================================================
-sleep 1
+sleep 3
 # generate fstab for filesystem mounts
 sudo touch /mnt/etc/fstab
 sudo chmod 777 /mnt/etc/fstab
@@ -43,10 +55,10 @@ clear
 
 # prep rc.local to set static IP
 echo Time to add a network configuration, we will assume dhcp but you can change the /etc/rc.local later if you want it to be static.
-sleep 1
+sleep 3
 ip link
 echo ""
-echo Add the name of your network adaptor e.g. "enp0s3" for virtualbox machines.
+echo Add the name of your network adaptor e.g. "enp0s3" for virtualbox machines and likely "eth0" for normal lan.
 echo -e "\e[1;32m ============================================ \e[0m "
 read -p 'Name of network adaptor: ' netname
 echo -e "\e[1;32m ============================================ \e[0m "
@@ -62,7 +74,7 @@ sudo echo "dhcpcd $netname" >> /mnt/etc/rc.local
 echo =============================================================================
 echo -e "Type \e[1;32m sh artix_final.sh \e[0m"
 echo =============================================================================
-sleep 2
+sleep 3
 # change root environtment
 artix-chroot /mnt
 
