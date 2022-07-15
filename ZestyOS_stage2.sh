619610ef455cec9022a790a5c218c792e9319113
Zesty <83487277+zestynotions@users.noreply.github.com>
2022-07-15 11:37:36 +0900

    adding cronie and ntp services

--- ZestyOS_stage2.sh
+++ ZestyOS_stage2.sh
@@ -60,8 +60,8 @@ sudo echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 
 
 # Further installs
-sudo pacman -Sq dhcpcd-runit rsm git neovim figlet noto-fonts ttf-fira-code openssh-runit tmux zsh --needed --noconfirm
-
+sudo pacman -Sq cronie-runit ntp-runit dhcpcd-runit rsm git neovim figlet noto-fonts ttf-fira-code openssh-runit tmux zsh --needed --noconfirm
+sudo ln -s /etc/runit/sv/ntp-runit /run/runit/ntp-runit/
 
 # change the default shell for user
 sudo echo Changing the default shell to ZSH
