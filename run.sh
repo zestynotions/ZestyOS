a74851b73d8d0966cbdc589a41ca78f611b4a45b
Zesty <83487277+zestynotions@users.noreply.github.com>
2022-07-17 14:43:52 +0900

    alpine up1

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ echo "Welcome to ZestyOS installer"
 echo ""
 ping -c3 google.com
 wget https://raw.githubusercontent.com/zestynotions/ZestyOS/main/alpine_setup_file
-
+setup-alpine -f alpine_setup_file
+reboot
